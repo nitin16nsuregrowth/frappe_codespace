f84bb53581857b236da682ac1204f35b2a3b2866
nitin16nsuregrowth <nitin@nsuregrowth.com>
2025-03-26 18:35:18 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,10 +11,10 @@ fi
 rm -rf /workspaces/frappe_codespace/.git
 
 source /home/frappe/.nvm/nvm.sh
-nvm alias default 20
-nvm use 20
+nvm alias default 18
+nvm use 18
 
-echo "nvm use 20" >> ~/.bashrc
+echo "nvm use 18" >> ~/.bashrc
 cd /workspace
 
 bench init \
